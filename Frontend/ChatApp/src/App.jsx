934d146971bac4d99a267331d9a66e521dd63818
murugadass-b <murugadassbala@gmail.com>
2025-05-21 00:09:23 +0530

    chat container added

--- Frontend/ChatApp/src/App.jsx
+++ Frontend/ChatApp/src/App.jsx
@@ -1,11 +1,18 @@
 import Header  from "./components/header"
 import FriendsList from "./components/friends-list"
+import ChatArea from "./components/chat-area"
+import { useState } from "react";
 
 export default function App(){
+  const [selectedFriend, setSelectedFriend] = useState(null);
   return (
     <>
     <Header/>
-    <FriendsList/>
+    <div className="chat-container">
+      <FriendsList setSelectedFriend={setSelectedFriend} selectedFriend={selectedFriend}/>
+      <ChatArea selectedFriend={selectedFriend}/>
+    </div>
+    
     </>
     
   )
